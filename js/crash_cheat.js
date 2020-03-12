221855190e36ab3e70c2f1c3f74bc50e0d6c9053
fangxi <loveinori@qq.com>
2020-03-12 18:01:46 +0800

    Site updated: 2020-03-12 18:01:46

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 11 2020 13:23:43 GMT+0800 (中国标准时间)
+// build time:Thu Mar 12 2020 18:01:45 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
